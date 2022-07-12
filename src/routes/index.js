8173ac3e0eba87671a19325e53b57b00d564f0f2
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-12 13:21:55 +0200

    send query and return data

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,49 @@
 import { client } from '$lib/graphql-client';
 import { gql } from 'graphql-request';
 
-import { astToHtmlString } from '@graphcms/rich-text-html-renderer';
-
 export const GRAPHQL_ENDPOINT = process.env['GRAPHQL_ENDPOINT'];
 console.log(GRAPHQL_ENDPOINT);
+
+// 1.
+// use the get function to fire a GET request when the page loads
+// https://kit.svelte.dev/docs/routing#endpoints
+export async function get() {
+	try {
+		// 2.
+		// copy the query from your API playground
+		// make sure it works in the API playground first before you copy it into your code
+		const getMessages = gql`
+			query PostsRichText {
+				posts {
+					richText {
+						raw
+					}
+				}
+			}
+		`;
+
+		// 3.
+		// then create a request on the graphql-client and feed it the query
+		const postsRichTextData = await client.request(getMessages);
+
+		// 4.
+		// the result should be your rich text data
+		console.log(postsRichTextData);
+
+		// 5.
+		// finally return this data from the server side to the frontend of your application
+		// do this by having the body property and feed it the "postsRichTextData"
+		// you got back from the request to the graphql-client you made above
+		return {
+			status: 200,
+			body: { postsRichTextData }
+		};
+	} catch (error) {
+		return {
+			status: 500,
+			body: {
+				error: 'Server error : ' + error
+			}
+		};
+	}
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,14 @@
 <script>
-	export const API_URL = process.env['JSON_PLACEHOLDER_ENDPOINT'];
-	console.log(API_URL);
+	// 6.
+	// finally make the "postsRichTextData" you return to the body available in the frontend of your application
+	export let postsRichTextData;
 </script>
 
 <h1 class="text-4xl py-4">Welcome to SvelteKit</h1>
 <p class="text-2xl py-3">
 	Visit <a class="text-red-400" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+
+<!-- 7. -->
+<!-- show the postsRichTextData data -->
+{JSON.stringify(postsRichTextData, null, 2)}
