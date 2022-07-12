487d4f049fb311cc754a898657340fd1a514d708
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-12 12:46:11 +0200

    :sparkles:

---
+++ src/routes/index.js
@@ -0,0 +1,2 @@
+import { client } from '$lib/graphql-client';
+import { gql } from 'graphql-request';
---
+++ svelte.config.js
@@ -0,0 +1,57 @@
+// import adapter from '@sveltejs/adapter-auto';
+
+// import adapter from '@sveltejs/adapter-node';
+
+import adapter from '@sveltejs/adapter-static';
+
+import path from 'path';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// Adapter Auto
+		// @sveltejs/adapter-auto
+		// adapter: adapter(),
+
+		// Adapter Node start
+		// https://github.com/sveltejs/kit/tree/master/packages/adapter-node
+		// adapter: adapter({ out: 'build' }),
+		// Adapter Node end
+
+		// Adapter Static start
+		// https://github.com/sveltejs/kit/tree/master/packages/adapter-static
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build'
+			// fallback: null,
+			// precompress: false
+		}),
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
+		// Adapter Static end
+		// vite: {
+		// 	server: {
+		// 		host: '0.0.0.0',
+		// 		port: 3000,
+		// 		strictPort: true,
+		// 		hmr: {
+		// 			clientPort: 443
+		// 		}
+		// 	},
+		// 	define: {
+		// 		// env-cmd https://blog.hdks.org/Environment-Variables-in-SvelteKit-and-Vercel/
+		// 		'process.env': process.env
+		// 	},
+		// 	resolve: {
+		// 		alias: {
+		// 			$root: path.resolve('./src')
+		// 		}
+		// 	}
+		// }
+	}
+};
+
+export default config;
