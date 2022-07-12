f485f2b120aef444167461cc147b2c408ea713d0
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-12 13:07:44 +0200

    import astToHtmlString

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import { client } from '$lib/graphql-client';
 import { gql } from 'graphql-request';
 
+import { astToHtmlString } from '@graphcms/rich-text-html-renderer';
+
 export const GRAPHQL_ENDPOINT = process.env['GRAPHQL_ENDPOINT'];
 console.log(GRAPHQL_ENDPOINT);
