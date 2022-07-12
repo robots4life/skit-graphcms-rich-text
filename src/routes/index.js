6cfdea2cf191a6340a5f1c0b3c826823d84c525f
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-12 12:56:09 +0200

    log GRAPHQL_ENDPOINT on server

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,2 +1,5 @@
 import { client } from '$lib/graphql-client';
 import { gql } from 'graphql-request';
+
+export const GRAPHQL_ENDPOINT = process.env['GRAPHQL_ENDPOINT'];
+console.log(GRAPHQL_ENDPOINT);
